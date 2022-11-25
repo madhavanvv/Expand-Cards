6cd76e54e03296e5ed30704515fed3c9dbe9e378
madhavanvv <madhavan1177@gmail.com>
2022-11-25 07:42:19 +0530

    two way card close open

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+//practice index
+
 const panels = document.querySelectorAll(".panel");
-//console.log(panels);
-panels.forEach((panel)=>{
 
-    panel.addEventListener('click',()=>{
-        removeActiceClass()
-        panel.classList.add("active");
-    })
+let invert = true;
 
-})
+function mySwap() {
+  if (invert) {
+    invert = false;
+    return "active";
+  }
 
-function removeActiceClass(){
-    panels.forEach((panel)=>{
-        panel.classList.remove('active')
-    })
+  invert = true;
+  return;
+}
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    let myVall = mySwap();
+    removeForAll();
+    panel.classList.add(myVall);
+  });
+});
+
+function removeForAll() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
